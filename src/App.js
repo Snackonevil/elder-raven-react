439e644bd5370877499488d49b985cc9333bdb53
Kevin Lacson <lacsonky@gmail.com>
2022-03-11 11:07:36 -0500

    footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import "./App.css";
 
@@ -16,6 +17,7 @@ function App() {
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
                 </Routes>
+                <Footer />
             </Router>
         </>
     );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import FooterLogo from "../images/logos/footer-logo.png";
+import { Link } from "react-router-dom";
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <img src={FooterLogo} alt="footer-logo" />
+            </div>
+            <div className="gray-line"></div>
+            <div className="footer-links">
+                <Link to="/about">ABOUT</Link>
+                <Link to="/contact">CONTACT</Link>
+                <a
+                    href="https://www.instagram.com/elder_raven_leatherworking/"
+                    target="_blank"
+                >
+                    INSTAGRAM
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
