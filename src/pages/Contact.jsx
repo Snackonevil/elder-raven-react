5b5053af6538bf9041ec323d312ade543c910d73
Kevin Lacson <lacsonky@gmail.com>
2022-03-12 09:10:44 -0500

    nav for contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,18 @@
 import Navbar from "../components/Navbar";
+import MobileNavbar from "../components/MobileNavbar";
+import { useContext } from "react";
+import WindowWidthContext from "../context/WindowWidthContext";
 
 function Contact() {
+    const deviceType = useContext(WindowWidthContext);
+
     return (
         <>
-            <Navbar />
+            {deviceType === "mobile" || deviceType === "tablet" ? (
+                <MobileNavbar />
+            ) : (
+                <Navbar />
+            )}
             <h1>Contact</h1>
         </>
     );
