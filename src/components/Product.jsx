ae0f11d866bb1e30e8bcdd5a9c38295115025f7e
Kevin Lacson <lacsonky@gmail.com>
2022-03-12 10:06:05 -0500

    Product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
-function Product() {
-    return <div className="project">Product</div>;
+function Product({ product }) {
+    let toggle = false;
+    const toggleProject = target => {
+        toggle = !toggle;
+        console.log(toggle);
+    };
+    return (
+        <div id="project-1" className="project inactive-project">
+            <div className="project-carousel">
+                <div className="project-carousel-track">
+                    {product.images.map((image, index) => {
+                        return (
+                            <img
+                                key={index}
+                                className="picture"
+                                src={image}
+                                alt="product"
+                            />
+                        );
+                    })}
+                </div>
+                <div className="project-carousel-btns hidden">
+                    <button className="project-left">
+                        <i className="fa-solid fa-chevron-left"></i>
+                    </button>
+                    <button className="project-right">
+                        <i className="fa-solid fa-chevron-right"></i>
+                    </button>
+                </div>
+            </div>
+            <div
+                className="overlay"
+                onClick={() => toggleProject(product.product_name)}
+            >
+                <div className="project-name">
+                    <h1>{product.product_name}</h1>
+                    <h1>{product.second_line}</h1>
+                </div>
+                <p className="project-description hidden">
+                    {product.description}
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default Product;
---
+++ src/images/products/index.js
@@ -0,0 +1,64 @@
+const ProductImages1 = [
+    require("./carousel-test2.jpg"),
+    require("./carousel-test3.jpg"),
+    require("./carousel-test1.jpg"),
+];
+const ProductImages2 = [
+    require("./carousel-test1.jpg"),
+    require("./carousel-test3.jpg"),
+    require("./carousel-test2.jpg"),
+];
+const ProductImages3 = [
+    require("./carousel-test2.jpg"),
+    require("./carousel-test1.jpg"),
+    require("./carousel-test3.jpg"),
+];
+
+const Products = [
+    {
+        product_name: "PRODUCT ONE",
+        second_line: "Second Line",
+        images: ProductImages1,
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. A quo corporis voluptatem officia nostrum illo
+        inventore similique dolorum, cupiditate sed quos
+        asperiores minima quas! Beatae dolor odit obcaecati
+        mollitia corporis. Iste, amet accusantium in doloremque
+        assumenda natus omnis molestias enim?`,
+    },
+    {
+        product_name: "PRODUCT TWO",
+        second_line: "Second Line",
+        images: ProductImages2,
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. A quo corporis voluptatem officia nostrum illo
+        inventore similique dolorum, cupiditate sed quos
+        asperiores minima quas! Beatae dolor odit obcaecati
+        mollitia corporis. Iste, amet accusantium in doloremque
+        assumenda natus omnis molestias enim?`,
+    },
+    {
+        product_name: "PRODUCT THREE",
+        second_line: "Second Line",
+        images: ProductImages3,
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. A quo corporis voluptatem officia nostrum illo
+        inventore similique dolorum, cupiditate sed quos
+        asperiores minima quas! Beatae dolor odit obcaecati
+        mollitia corporis. Iste, amet accusantium in doloremque
+        assumenda natus omnis molestias enim?`,
+    },
+    {
+        product_name: "PRODUCT FOUR",
+        second_line: "Second Line",
+        images: ProductImages1,
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. A quo corporis voluptatem officia nostrum illo
+        inventore similique dolorum, cupiditate sed quos
+        asperiores minima quas! Beatae dolor odit obcaecati
+        mollitia corporis. Iste, amet accusantium in doloremque
+        assumenda natus omnis molestias enim?`,
+    },
+];
+
+export default Products;
